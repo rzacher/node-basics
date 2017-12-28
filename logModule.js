337662cb2a3cebf6e147766f59561f19b0aa22bc
bzacher100@gmail.com <bzacher100@gmail.com>
2017-12-28 17:16:44 -0500

    Added logModule.js

---
+++ logModule.js
@@ -0,0 +1,14 @@
+var log = {
+		info: function(info)  {
+			console.log(info);
+		},
+		warning: function(warning) {
+			console.log(warning);
+		},
+		error: function(error) {
+			console.log(error)
+		}
+		
+};
+
+module.exports = log; 
